10ef34773f3ce3b353e8aa5aec0534149ef3a71a
Marcelo XP <marceloxp@gmail.com>
2022-10-03 15:17:19 -0300

    - Min fix.

--- app/Utilities/Brasil.php
+++ app/Utilities/Brasil.php
@@ -4,11 +4,22 @@ namespace App\Utilities;
 
 class Brasil
 {
+    /**
+     * Get all states.
+     *
+     * @return \App\Utilities\Result
+     */
     public static function getStates()
     {
         return \App\Models\State::getAll();
     }
 
+    /**
+     * Get the cities by UF.
+     *
+     * @param  String  $p_uf
+     * @return \App\Utilities\Result
+     */
     public static function getCitiesByUf($p_uf)
     {
         return \App\Models\City::getByUf($p_uf);
