fc911ed25171b8161f1ecfda853685a848abb72d
Marcelo XP <marceloxp@gmail.com>
2022-10-16 15:11:44 -0300

    - Adjust https.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
     }
 }
