83c2fe2f7cd133f88598fa15f6abefe7ac862ea5
Marcelo XP <marceloxp@gmail.com>
2024-03-07 18:35:21 -0300

    - Min adjusts

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -12,7 +12,7 @@
         <meta name="app-version" content="{{ app_version() }}">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
         <x-favicon url="/favicon.png"/>
-        <x-css src="/css/style.css"/>
+        <x-css src="/css/style.min.css"/>
 
         {{ MetaSocial::print() }}
         @section('vendor.css')
